9fa091e0c558dcbaeda8a3b29ada86667a5b23fb
May Carandang <maytiara@gmail.com>
2022-07-28 01:50:43 +0800

    Added property for getId using .toBe value(assert)

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,15 +5,27 @@ describe ("Employee Test", () => {
     test("getName() will return the name property", () => {
 
         //Arrange -- prepare the env
-        const name = "Karen";
-
-        const dummy = new Employee (1, 'karen@anonymous.com', name);
+        const name = "Name";
+        const nameTest = new Employee (1, 'karen@anonymous.com', name);
 
         //act
-        const result = dummy.getName();
+        const result = nameTest.getName();
 
         //assert
         expect(result).toStrictEqual(name);
+    })
+
+    test("getId() will return the id property", () => {
+
+        //Arrange -- prepare the env
+        const id = "89556";
+        const idTest = new Employee (1, '895656', id);
+
+        //act
+        idTest.getId('test Id');
+
+        //assert
+        expect(id).toBe(id); //toBe works! as it says it uses === to check strict equality
     });
 
 })
