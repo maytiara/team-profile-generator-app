3b9bc19a5b962d21a97fa42c542fce78ecf85e69
May Carandang <maytiara@gmail.com>
2022-07-28 02:12:27 +0800

    Added test for Engineer ( ) same syntax & changed few lines

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,70 @@
+const Engineer = require("../src/employees/Engineer");
+
+describe ("Engineer Test", () => {
+
+    test("getName() will return the name property", () => {
+
+        //Arrange -- prepare the env
+        const name = "Name";
+        const nameTest = new Engineer (1, 'karen', name);
+
+        //act
+        const result = nameTest.getName();
+
+        //assert
+        expect(result).toStrictEqual(name);
+    })
+
+    test("getId() will return the id property", () => {
+
+        //Arrange -- prepare the env
+        const id = "89556";
+        const idTest = new Engineer (1, '895656', id);
+
+        //act
+        idTest.getId('test Id');
+
+        //assert
+        expect(id).toBe(id); //toBe works! as it says it uses === to check strict equality
+    })
+
+    test("getEmail() will return the email property", () => {
+
+        //Arrange -- prepare the env
+        const email = "karen@dot.com";
+        const emailTest = new Engineer (1, 'karen@dot.com', email);
+
+        //act
+        emailTest.getEmail('test Email');
+
+        //assert
+        expect(email).toBe(email); //toBe works! as it says it uses === to check strict equality
+    })
+
+    test("getRole() will return the role property", () => {
+
+        //Arrange -- prepare the env
+        const role = "Founder";
+        const roleTest = new Engineer (1, 'Founder', role);
+
+        //act
+        roleTest.getEmail('test Role');
+
+        //assert
+        expect(role).toBe(role); //toBe works! as it says it uses === to check strict equality
+    })
+
+    test("getGithub() will return the office_number property", () => {
+
+        //Arrange -- prepare the env
+        const github = "github.com/beta";
+        const githubTest = new Engineer (1, 'github.com/beta', github);
+
+        //act
+        githubTest.getGithub('github');
+
+        //assert
+        expect(github).toBe(github); //toBe works! as it says it uses === to check strict equality
+    });
+
+})
