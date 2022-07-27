d157a5c03ca3f7a45c0d8d9c2803acd0a024e03f
May Carandang <maytiara@gmail.com>
2022-07-27 21:43:52 +0800

    Added func to generate the card for each employee

--- src/generate-html/html.js
+++ src/generate-html/html.js
@@ -1,10 +1,22 @@
+const fs = require('fs'); //default variable
+const path = require('path'); //default variable
 
+function generateCard(employee) { // function to generate the card for each employee => const cards
+
+    // scanned the card.html template
+
+    // replace the placeholders with the actual input data
+}
 
 function generateHtml (employees) {
 
     // loop through each employee
 
-    // generate 
+    // generate a card for each employee
+    const cards = employees.map(generateCard)
+    // after the loop, once done we will join the cards into one stack
+
+    // replace the {{body}} with the card string
 
 }
 
