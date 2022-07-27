3d3cddf2997447179cbde2d31a701e0d21ae1ca1
May Carandang <maytiara@gmail.com>
2022-07-27 21:53:35 +0800

    syntax to replace the default data for every role

--- src/generate-html/html.js
+++ src/generate-html/html.js
@@ -1,11 +1,17 @@
 const fs = require('fs'); //default variable
 const path = require('path'); //default variable
 
+const cardTemplatePath = path.join(__dirname, 'demo-templates', 'card-container.html');
+
+// This function generates the input visible for every role.
 function generateCard(employee) { // function to generate the card for each employee => const cards
 
-    // scanned the card.html template
-
+    // reads the entire contents of card.html template
+    const cardTemplate = fs.readFileSync(cardTemplatePath, 'utf-8');
     // replace the placeholders with the actual input data
+    const replacedData = cardTemplate.replace('{{name}}', employee.getName())
+        .replace('{{id}}', employee.getId())
+        .replace('{{email}}', employee.getEmail())
 }
 
 function generateHtml (employees) {
