c1852d531317fa6fe8f9ec7e0221deef337c2c6e
May Carandang <maytiara@gmail.com>
2022-07-27 14:22:06 +0800

    Added boolean condion for function base()

--- app.js
+++ app.js
@@ -35,15 +35,15 @@ async function base () { //function for question set to be asked inside node.js
 			message: 'What is the office number?',
 			name: 'office_number',
 			when: function (answers) { //return function that returns true statement
-				return answers.role === 'manager'
-			}
+				return answers.role === 'manager';
+			},
 		},
 		{
 			type: 'input',
 			message: 'What is the github username/link?',
 			name: 'github',
 			when: function (answers) { //return function that returns true statement
-				return answers.role === 'engineer'
+				return answers.role === 'engineer';
 			}
 		},
 		{
@@ -51,11 +51,23 @@ async function base () { //function for question set to be asked inside node.js
 			message: 'What is the school/institution name?',
 			name: 'school',
 			when: function (answers) { //return function that returns true statement
-				return answers.role === 'intern'
-			}
+				return answers.role === 'intern';
+			},
+		},
+		{
+			type: 'confirm',
+			message: 'Do you want to register another employee?',
+			name: 'register',
 		},
   ])
 
+	// This is the condition, once we have all the input questions has been answered.
+	// We will have another prompt asking if the user want to register another employee. 
+  	if (!answers.register) { //TRUE, statement
+		//FALSE, statement once the user answered NO
+	} 	else {
+		base(); //This a recursive approach, that let user to repeats the same question in a loop cycle.
+	}
 }
 
 base(); //to run the function
